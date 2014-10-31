ee8df40d30059d52c484e6edd1a091072137a8b6
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2014-11-01 02:30:08 +0530

    autoscroll on event page hash

--- events.php
+++ events.php
@@ -73,7 +73,7 @@
             foreach ($cat_data as $name=>$event) {
           ?>
           <div class="circle-wrapper">
-            <div class="outer-circle" id="<?php echo classname($name); ?>">
+            <div class="outer-circle" id="<?php echo classname($name); ?>-id">
               <div class="inner-circle">
                 <div class="circle-content-wrapper">
                   <div class="circle-content"><?php echo $name; ?></div>
