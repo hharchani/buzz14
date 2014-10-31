3a7c351991f59541dea946ad54c6f1f35a65e139
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2014-10-31 22:26:43 +0530

    Format of `data.php`

---
+++ data.php
@@ -0,0 +1,13 @@
+<?php
+$events = array(
+  "Threads" => array(
+    "Hackathon" => array(
+      "description" => "",
+      "time" => array( array( 3, 22, 23 ), array( 4, 00, 14 ) ),
+    ),
+  ),
+  "Pulsation" => array(
+  ),
+  "Zombie Zone" => array(
+  ),
+);
