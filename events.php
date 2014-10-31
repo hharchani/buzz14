af1c7f1d34e8ae728065deb8b343549b637e1634
Nisarg Jhaveri <nisargjhaveri@gmail.com>
2014-11-01 00:05:55 +0530

    Event page

--- events.php
+++ events.php
@@ -1,5 +1,36 @@
 <?php
   $current_page = "events";
+  include_once("data.php");
+
+  function getHour($hour) {
+    $h = floor($hour);
+    if($h==14)$h=0;
+    $part = $hour-$h;
+    $m = floor($part * 60);
+    $a = ($h >= 12)?'PM':'AM';
+    $h -= ($h >= 12)?12:0;
+    return $h.':'.$m.' '.$a;
+  }
+
+  function getTime($times) {
+    $days = array(
+      0 => "Friday 31<sup>st</sup> October",
+      1 => "Saturday 1<sup>st</sup> November",
+      2 => "Sunday 2<sup>nd</sup> November",
+      3 => "Monday 3<sup>rd</sup> November",
+      4 => "Tuesday 4<sup>th</sup> November",
+    );
+    $timeStr = "";
+    foreach ($times as $i=>$time) {
+      if (!($i > 0 && $time[0] == $times[$i-1][0]+1 && $time[1] == 0 && $times[$i-1][2] == 23)) {
+        $timeStr .= "<br>".$days[$time[0]].", ".getHour($time[1]);
+      }
+      if (!($i < count($times)-1 && $time[0] == $times[$i+1][0]-1 && $time[2] == 23 && $times[$i+1][1] == 0)) {
+        $timeStr .= " - ".$days[$time[0]].", ".getHour($time[2]+1);
+      }
+    }
+    return $timeStr;
+  }
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,8 +38,8 @@
   <title>Events | Felicity Buzz</title>
   <meta charset="utf-8">
   <link href='http://fonts.googleapis.com/css?family=Loto|Rock+Salt' rel='stylesheet' type='text/css'>
-  
-  <!-- Jquery | Include from Google 
+
+  <!-- Jquery | Include from Google
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   <!-- If jquery unavailable from Google, get a local copy -->
   <script>
@@ -21,91 +52,54 @@
 </head>
 <body>
   <?php include_once('header.php'); ?>
-  <div class="container">
+  <div class="container" id='top-container'>
     <h1 class="heading">Events</h1>
     <hr />
-    
+
+    <?php
+      foreach ($events_cat as $cat_name=>$cat_data) {
+    ?>
     <div class="row">
       <div class="sub-heading-wrapper">
-        <h3 class="sub-heading">Zombie Zone</h3>
-      </div>
-      <div class="circle-container no-user-select">
-        
-        <div class="circle-wrapper">
-          <div class="outer-circle">
-            <div class="inner-circle">
-              <div class="circle-content-wrapper"><div class="circle-content">Counter Strike 1.6</div></div>
+        <h3 class="sub-heading"><?php echo $cat_name; ?></h3>
+        <div class="circle-container no-user-select">
+          <?php
+            foreach ($cat_data as $name=>$event) {
+          ?>
+          <div class="circle-wrapper">
+            <div class="outer-circle">
+              <div class="inner-circle">
+                <div class="circle-content-wrapper">
+                  <div class="circle-content"><?php echo $name; ?></div>
+                  <div class="circle-des"><?php echo $event['description']; ?></div>
+                  <div class="circle-time"><?php echo getTime($event['time']); ?></div>
+                  <div class="circle-venue"><?php echo $event['venue']; ?></div>
+                  <div class="circle-link"><?php echo isset($event['link'])?$event['link']:''; ?></div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div class="circle-wrapper">
-          <div class="outer-circle">
-            <div class="inner-circle">
-              <div class="circle-content-wrapper"><div class="circle-content">Fifa 2014</div></div>
-            </div>
-          </div>
-        </div>
-        <div class="circle-wrapper">
-          <div class="outer-circle">
-            <div class="inner-circle">
-              <div class="circle-content-wrapper"><div class="circle-content">Call of duty MW1</div></div>
-            </div>
-          </div>
-        </div>
-				<div class="circle-wrapper">
-          <div class="outer-circle">
-            <div class="inner-circle">
-              <div class="circle-content-wrapper"><div class="circle-content">NFS Most Wanted</div></div>
-            </div>
-          </div>
-        </div>
-				<div class="circle-wrapper">
-          <div class="outer-circle">
-            <div class="inner-circle">
-              <div class="circle-content-wrapper"><div class="circle-content">DOTA 2</div></div>
-            </div>
-          </div>
-        </div>
-				
-      </div>
-    </div>
-    <hr />
-    <!-- ----------------------------------------------------------------------- -->
-    <div class="row">
-      <div class="sub-heading-wrapper">
-        <h3 class="sub-heading">Zombie zone</h3>
-      </div>
-      <div class="circle-container no-user-select">
-        
-        <div class="circle-wrapper">
-          <div class="outer-circle">
-            <div class="inner-circle">
-              <div class="circle-content-wrapper"><div class="circle-content">Hackathon</div></div>
-            </div>
-          </div>
-        </div>
-        <div class="circle-wrapper">
-          <div class="outer-circle">
-            <div class="inner-circle">
-              <div class="circle-content-wrapper"><div class="circle-content">Break In</div></div>
-            </div>
-          </div>
-        </div>
-        <div class="circle-wrapper">
-          <div class="outer-circle">
-            <div class="inner-circle">
-              <div class="circle-content-wrapper"><div class="circle-content">Code Craft</div></div>
-            </div>
-          </div>
+          <?php
+            }
+          ?>
         </div>
       </div>
     </div>
     <hr />
-    <!-- ----------------------------------------------------------------------- -->
-    
+    <?php
+      }
+    ?>
   </div>
   <div class="full-overlay"></div>
-  <div class="overlay"></div>
+  <div class="overlay">
+    <div class="overlay-content-wrap">
+      <div class="overlay-title"></div>
+      <div class="overlay-des"></div>
+      <div class="overlay-time"></div>
+      <div class="overlay-venue"></div>
+      <div class="overlay-link"></div>
+    </div>
+  </div>
   <script src="js/events.js"></script>
 </body>
 </html>
